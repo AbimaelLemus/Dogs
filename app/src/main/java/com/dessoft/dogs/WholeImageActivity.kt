fdcf30e8b9f22f33eeb9a4d6f60599200ce9dfe2
perezba <perezbiab@globalhitss.com>
2023-04-03 16:27:12 -0600

    v49 show photo

--- app/src/main/java/com/dessoft/dogs/WholeImageActivity.kt
+++ app/src/main/java/com/dessoft/dogs/WholeImageActivity.kt
@@ -1,11 +1,35 @@
 package com.dessoft.dogs
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import coil.load
+import com.dessoft.dogs.databinding.ActivityWholeImageBinding
+import java.io.File
 
 class WholeImageActivity : AppCompatActivity() {
+
+    companion object {
+        const val PHOTO_URI_KEY = "photo_uri"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_whole_image)
+        val binding = ActivityWholeImageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val photoUri = intent.extras?.getString(PHOTO_URI_KEY)
+        val uri = Uri.parse(photoUri)
+        val path = uri.path
+
+        if (path == null) {
+            Toast.makeText(this, "Error showing image no photo uri", Toast.LENGTH_LONG).show()
+            finish()
+            return
+        }
+
+        binding.ivWhole.load(File(path))
+
     }
 }
