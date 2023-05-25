15a4fd451b8628fc31a2209a8c1d75995dae722e
perezba <perezbiab@globalhitss.com>
2023-05-25 15:28:35 -0600

    v102 creating a simple Unit Test

--- app/src/test/java/com/dessoft/dogs/ExampleUnitTest.kt
+++ app/src/test/java/com/dessoft/dogs/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.dessoft.dogs
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +9,23 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    /**
+     * Para el nombre de los metodos
+     * primero lo que se va a hacer
+     * Y luego el resultado de lo que debemos de obtener
+     * y arriba del nombre agregar la anotación de @Test
+     *
+     * el assert y sus derivados
+     *
+     * testImplementation 'junit:junit:4.13.2'
+     * androidTestImplementation 'androidx.test.ext:junit:1.1.5'
+     * androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.1'
+     */
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun additionIsCorrect() {
+        assertEquals(4, 2 + 1)
     }
+
+
 }
